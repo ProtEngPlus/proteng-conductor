c79de058c896cae8ea184e78021d56bc7db152d0
Chanawat Voravijitchaikul <nanae11879@gmail.com>
2023-11-21 22:18:07 +0700

    add lab_result to request body to fit_top

--- models/job.go
+++ models/job.go
@@ -6,12 +6,18 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+type LabResult struct {
+	Total     int      `bson:"total" json:"total"`
+	Sequences []string `bson:"sequences" json:"sequences"`
+	Scores    []string `bson:"scores" json:"scores"`
+}
+
 type Job struct {
 	Id           primitive.ObjectID     `bson:"_id" json:"id"`
 	State        string                 `bson:"state" json:"state"`
 	StageId      int                    `bson:"stage_id" json:"stage_id"`
 	UserId       string                 `bson:"user_id" json:"user_id"`
-	LabResult    map[string]interface{} `bson:"lab_result" json:"lab_result"`
+	LabResult    LabResult              `bson:"lab_result" json:"lab_result"`
 	Options      map[string]interface{} `bson:"options" json:"options"`
 	Artifacts    map[string]interface{} `bson:"artifact" json:"artifact"`
 	Meta         []string               `bson:"meta" json:"meta"`

6f2ae03c3620eccb2ae34d5bfb0b8c65ab287666
Chanawat Voravijitchaikul <nanae11879@gmail.com>
2024-01-04 20:31:02 +0700

    fix result type

--- models/mutation.go
+++ models/mutation.go
@@ -12,7 +12,7 @@ type Mutation struct {
 	InputProtein string                 `bson:"input_protein" json:"input_protein"`
 	Options      map[string]interface{} `bson:"options" json:"options"`
 	State        string                 `bson:"state" json:"state"`
-	Result       map[string]int         `bson:"lab_result" json:"lab_result"`
+	Result       map[string]float32     `bson:"result" json:"result"`
 	CreatedAt    time.Time              `bson:"created_at" json:"created_at"`
 	CompleteAt   time.Time              `bson:"complete_at" json:"complete_at"`
 }

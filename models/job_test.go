4783c7b2d4189994387fd02f0787cb942a8b7f6f
matikant <fe-2003@hotmail.com>
2025-01-05 22:12:17 +0700

    fix: test remove 'Names' from LabResult

--- models/job_test.go
+++ models/job_test.go
@@ -12,7 +12,6 @@ func TestValidateLabResult(t *testing.T) {
 	t.Run("valid", func(t *testing.T) {
 		lr := LabResult{
 			Total:     3,
-			Names:     []string{"a", "b", "c"},
 			Sequences: []string{"a", "b", "c"},
 			Scores:    []float32{1.0, 2.0, 3.0},
 		}
@@ -23,7 +22,6 @@ func TestValidateLabResult(t *testing.T) {
 	t.Run("invalid length mismatch", func(t *testing.T) {
 		lr := LabResult{
 			Total:     3,
-			Names:     []string{"a", "b", "c"},
 			Sequences: []string{"a", "b", "c"},
 			Scores:    []float32{1.0, 2.0},
 		}
@@ -34,7 +32,6 @@ func TestValidateLabResult(t *testing.T) {
 	t.Run("invalid total", func(t *testing.T) {
 		lr := LabResult{
 			Total:     2,
-			Names:     []string{"a", "b", "c"},
 			Sequences: []string{"a", "b", "c"},
 			Scores:    []float32{1.0, 2.0, 3.0},
 		}
@@ -51,7 +48,7 @@ func TestValidateJob(t *testing.T) {
 			Name:             "test",
 			StageId:          1,
 			UserId:           "test",
-			LabResult:        LabResult{Total: 3, Names: []string{"a", "b", "c"}, Sequences: []string{"a", "b", "c"}, Scores: []float32{1.0, 2.0, 3.0}},
+			LabResult:        LabResult{Total: 3, Sequences: []string{"a", "b", "c"}, Scores: []float32{1.0, 2.0, 3.0}},
 			Options:          map[string]interface{}{"a": 1},
 			Artifacts:        map[string]Artifact{"a": {BucketName: "test", Path: "test", Url: "test"}},
 			Meta:             []string{"test"},

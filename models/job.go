63bb9c71fe50d3366f35ad16ba67624b7672b92d
BadLuckZ <thanagorn8802@gmail.com>
2026-08-25 23:52:30 +0700

    fix: remove unused names field from lab result

--- models/job.go
+++ models/job.go
@@ -12,13 +12,12 @@ import (
 
 type LabResult struct {
 	Total     int       `bson:"total" json:"total" validate:"gte=0"`
-	Names     []string  `bson:"names" json:"names" validate:"required"`
 	Sequences []string  `bson:"sequences" json:"sequences" validate:"required"`
 	Scores    []float32 `bson:"scores" json:"scores" validate:"required"`
 }
 
 func (lr LabResult) Validate() error {
-	if lr.Total != len(lr.Sequences) || lr.Total != len(lr.Scores) || lr.Total != len(lr.Names) {
+	if lr.Total != len(lr.Sequences) || lr.Total != len(lr.Scores) {
 		return fmt.Errorf("data length mismatch")
 	}
 	return nil
